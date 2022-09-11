8ae5c4c47fb6ca2da296e893d30750fae77b8c64
anish0803 <105308559+anish0803@users.noreply.github.com>
2022-09-11 15:03:58 +0530

    fourth commit

--- script.js
+++ script.js
@@ -1,19 +1,37 @@
+let searchInputTxt = document.getElementById('search-input').value.trim();
 const searchBtn = document.getElementById('search-btn');
 const mealList = document.getElementById('meal');
 
 // event listeners
 searchBtn.addEventListener('click', ()=>{
 
-    fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?i=${searchInput.value}`)
+    fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?i=${searchInputTxt}`)
     .then(response => response.json())
     .then(data => {
 console.log(data)
-});
 
-});
+let result = "";
+        if(data.meals){
+            data.meals.forEach(meal => {
+                result += `
+                    <div class = "meal-item" data-id = "${meal.idMeal}">
+                        <div class = "meal-img">
+                            <img src = "${meal.strMealThumb}" alt = "food">
+                        </div>
+                        <div class = "meal-name">
+                            <h3>${meal.strMeal}</h3>
+                            <a href = "#" class = "recipe-btn">Get Recipe</a>
+                        </div>
+                    </div>
+                `;
+            });
+            mealList.classList.remove('notFound');
+        } else{
+            result = "Sorry, we didn't find any meal!";
+            mealList.classList.add('notFound');
+        }
 
+        mealList.innerHTML = result;
+    });
 
-
-
-
-
+})
